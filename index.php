13a0c7f7974cfb76167f6f98b602994ac5be81f4
mohahassan <mohamedfanax01@gmail.com>
2020-07-30 13:36:29 +0300

    index header url

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-        <title>Ladan Travel Agency</title>
+        <title>Ladan Travel Agency Mogadishu</title>
 
         <link href="Assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="Assets/font-awesome/css/font-awesome.css" rel="stylesheet">
